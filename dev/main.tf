8bb469ad5074cb981d951758f30bf12ceb9619cc
nhamcloudlab <nham.cloud.lab@gmail.com>
2023-07-26 22:20:23 +0700

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -13,3 +13,27 @@ resource "null_resource" "test_de333" {
 resource "null_resource" "test_de4444" {
 
 }
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
