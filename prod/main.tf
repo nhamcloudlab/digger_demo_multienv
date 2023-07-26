266206fc4b0cfff8a0116e8348a75280ca65397e
nhamcloudlab <nham.cloud.lab@gmail.com>
2023-07-26 22:47:36 +0700

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -5,3 +5,27 @@ resource "null_resource" "test_prod" {
 resource "null_resource" "test_prod2223" {
 
 }
+
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
